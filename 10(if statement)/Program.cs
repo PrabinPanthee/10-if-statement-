4fe1c6d2fa29cc9b1a15e83661a3446348f9a378
iampr <iampr@PRABIN>
2023-01-04 19:10:14 +0545

    Add project files.

---
+++ 10(if statement)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Write your number : ");
+        int number = int.Parse(Console.ReadLine ());
+        if (number == 1) 
+        {
+            Console.WriteLine("your number is one");
+
+        }
+         else if (number == 2)
+         {
+            Console.WriteLine("your number is two ");
+
+         }
+         else if  (number == 3)
+         {
+            Console.WriteLine("your number is three");
+         } 
+        else 
+        {
+            Console.WriteLine("your number is not here");
+        }
+         
+
+    }
+}
